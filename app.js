0269bf4ec4a2f0f78573968de9038537a650eb23
meghalojha <25003501610129@mail.ljku.edu.in>
2026-03-24 23:28:22 +0530

    Add emergency call and hospital functions

---
+++ app.js
@@ -0,0 +1,15 @@
+function callEmergency(){
+window.location.href="tel:108"
+}
+
+function openHospital(){
+window.open("https://www.google.com/maps/search/hospital+near+me")
+}
+
+function confirmAppointment(){
+alert("Appointment Booked Successfully")
+}
+
+function addMedicine(){
+alert("Medicine Added to Cart")
+}
