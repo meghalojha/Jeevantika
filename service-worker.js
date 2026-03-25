60418f9f61deec7c9eb7d044db35cb26fcaebe79
meghalojha <25003501610129@mail.ljku.edu.in>
2026-03-25 08:34:04 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,26 +1,19 @@
-const CACHE_NAME = "jeevantika-cache-v1";
+const CACHE="jeevantika-v1";
 
-const urlsToCache = [
+const ASSETS=[
 "/Jeevantika/",
 "/Jeevantika/index.html",
 "/Jeevantika/doctors.html",
-"/Jeevantika/doctor.html",
 "/Jeevantika/pharmacy.html",
 "/Jeevantika/profile.html"
 ];
 
-self.addEventListener("install", event => {
-event.waitUntil(
-caches.open(CACHE_NAME)
-.then(cache => cache.addAll(urlsToCache))
-);
+self.addEventListener("install",e=>{
+e.waitUntil(
+caches.open(CACHE).then(cache=>cache.addAll(ASSETS))
+)
 });
 
-self.addEventListener("fetch", event => {
-event.respondWith(
-caches.match(event.request)
-.then(response => {
-return response || fetch(event.request);
-})
-);
-});
+self.addEventListener("fetch",e=>{
+e.respondWith(
+caches.match(e.request).then
